3e3030e7da66c8ad83dc6b8bcb3eb55b9aad7b5e
vinayakUs <vinayak200054321@gmail.com>
2025-06-19 19:33:22 +0530

    initial

---
+++ default-auth/src/main/java/org/example/defaultauth/DefaultAuthApplication.java
@@ -0,0 +1,13 @@
+package org.example.defaultauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DefaultAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DefaultAuthApplication.class, args);
+    }
+
+}
---
+++ default-auth/src/main/java/org/example/defaultauth/SecurityConfig.java
@@ -0,0 +1,7 @@
+package org.example.defaultauth;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SecurityConfig {
+}
